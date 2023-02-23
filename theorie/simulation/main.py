71d3b3e315f5dc4f27053081cd6b1047045df98d
Firefnix <malo.leroy2662@gmail.com>
2023-02-23 16:31:20 +0100

    Squelette pour une simulation à 3D

--- theorie/simulation/main.py
+++ theorie/simulation/main.py
@@ -1,6 +1,29 @@
 import numpy as np
 
-h = 1
 
-def f_newton():
-    return h*(Text())
+# Carcasse de l'ordinateur
+Lx, Ly, Lz = 10e-2 # m : 10 cm
+c_ord = 1000 # J.K^-1.kg^-1
+V_ord = Lx * Ly * Lz
+rho_ord = 1000 # kg.m^-3
+S_ord = (2 * Lx * Ly) + (2 * Ly * Lz) + (2 * Lx * Lz)
+
+# Carcasse du processeur
+# T désigne la température du processeur
+lx, ly, lz = 1e-2 # m : 1 cm
+c_proc = 1000 # J.K^-1.kg^-1
+V_proc = lx * ly * lz
+rho_proc = 1000 # kg.m^-3
+S_proc = (2 * lx * ly) + (2 * ly * lz) + (2 * lx * lz)
+
+# Discrétisation du temps et de l'espace
+dd = Lx / 100
+dt = 1000 # s
+
+# Salle (air)
+
+U = 5 # V
+def I(K, T):
+    return ...
+def P(K, T): return U * I(K, T)
+def Pv(K, T): return P(K, T) / V_proc
