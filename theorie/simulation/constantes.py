c1d70a349e5bcefb3a77147a3e12ceb7b2f16a63
Firefnix <malo.leroy2662@gmail.com>
2023-02-23 21:09:07 +0100

    Remaniement de l'affichage

--- theorie/simulation/constantes.py
+++ theorie/simulation/constantes.py
@@ -10,7 +10,6 @@ T0 = kelvin(25) # °C
 
 # Masses volumiques
 rho = 2_330 # kg.m^-3 | wikipedia:silicium à 25°C
-print(f'ρ = {rho} kg.m^-3')
 def rho_a(T): # K -> kg.m^-3
     return 346.384 / T
 rho_a0 = rho_a(T0)
@@ -33,12 +32,12 @@ def Da(T): # K -> m^2/s
     return Ga(T) / (rho_a(T) * c_a)
 Da0 = Da(T0)
 D = G / (rho * c)
-print(f'D = {D:.2e} m^2/s')
 
-"""
-for ti in range(Nt):
-    for xi in range(1, Nx):
-        T[xi, ti+1] = (T[xi, ti] + (Te / (Xe**2)) * Dx(xi) *
-            (T[xi+1, ti] - T[xi, ti] + T[xi-1, ti])
-            + P_c(xi) * Te)
-"""
+if __name__ == '__main__':
+    print(f'T0 = {T0} K = {celsius(T0)} °C')
+    print(f'ρ = {rho} kg.m^-3')
+    print(f'ρ_a = {rho_a0:.2f} kg.m^-3 [à T0]')
+    print(f'λ = {G} W.m^-1.K^-1')
+    print(f'λ_a = {Ga0:.2e} W.m^-1.K^-1 [à T0]')
+    print(f'D = {D:.2e} m^2/s')
+    print(f'D_a = {Da0:.2e} m^2/s [à T0]')
