d346da50943e36f02751a423010364e26659e1ae
Firefnix <malo.leroy2662@gmail.com>
2023-05-29 12:51:53 +0200

    Prise en compte des bords

--- theorie/gradient/Debut-algo.py
+++ theorie/gradient/Debut-algo.py
@@ -1,29 +1,47 @@
 import numpy as np
-# Grandeurs
-xo = np.array([10.]) # point initial
-epsi = 0.01 # epsilon
-pas = 0.01
+
+#grandeurs
+xo = np.array([0.1]) #point initial
+epsi = 0.0001 #epsilon
+pas = 0.0001
 nbrtourspc = 5
-x = 10 # m
-y = 10 # m
-K = 1000 # calculs par secondes
+#on suppose avoir que des nombres entre 0 et 1
 
 
 #définitions fonction de bases
 def f(tab):
-    return (tab[0] - 3)**2
+    return (tab[0] - 1/2)**2
+
+def verif_point(p):
+    for i in p:
+        if i > 1 :
+            return False
+    return True
+
+def point_valide(p, grad, pas):
+    n = len(p)
+    for i in range(n):
+        if (p[i] - pas * grad[i]) < 0 or (p[i] - pas * grad[i]) > 1:
+            grad[i] = 0
+    return grad
 
 def iemederive_part(fonc, point, i, pas):
     pnew = np.copy(point)
-    pnew[i] = point[i] + pas
-    return (fonc(pnew) - fonc(point)) / pas
+    ieme = point[i] + pas
+    if ieme < 1:
+        pnew[i] = ieme
+        return (fonc(pnew) - fonc(point)) / pas
+    else:
+        pnew[i] = point[i]-pas
+        return -(fonc(pnew) - fonc(point)) / pas
+
 
 def gradient(fonc, pas, point):
     n = len(point)
     grad = np.zeros(n)
     for i in range(n):
         grad[i] = iemederive_part(fonc, point, i, pas)
-    return grad
+    return point_valide(point, grad, pas)
 
 def norme (vect):
     somme = 0
@@ -42,4 +60,4 @@ def v1grad(f, pas, xo, epsi):
         point = point - pas * nablaf
     return point
 
-print('Minimum atteint en :', v1grad(f, pas, xo, epsi))
+print(v1grad(f, pas, xo, epsi))
